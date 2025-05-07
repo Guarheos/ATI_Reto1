636553c37bfc1e09cf24c69de711dbcf844108f1
guarheos@protonmail.com <guarheos@protonmail.com>
2025-05-07 18:07:48 -0400

    listado de estudiantes dinámico usando javascript

--- js/index.js
+++ js/index.js
@@ -3,8 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('../reto3/conf/configES.json')
     .then(response => response.json())
     .then(datos => {
-        document.title = datos.sitio;
+        //Poner datos del JSON al title y footer
+        document.title = datos.sitio.join(" ");
         const footer = document.querySelector("footer");
         footer.textContent= datos.copyRight;
+
+        //Dummies dinamicos
+        fetch("../reto3/datos/index.json")
+            .then(response => response.json())
+            .then(datosdummies => {
+                const dummies = document.getElementById("dummies");
+                //dummies.innerHTML = "";
+                datosdummies.forEach(dummy => {
+                    const li = document.createElement("li");
+                    li.classList.add("dummy");
+
+                    li.innerHTML = `
+                        <img src="${dummy.imagen}" alt="Foto de perfil">
+                        <p class="nombreI">
+                            <a href="${dummy.perfil}">${dummy.nombre}</a>
+                        </p>
+                    `;
+
+                    dummies.appendChild(li);
+                });
+            })
+            .catch(error => console.error("Error cargando el JSON:", error));
     });
 });

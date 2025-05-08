48acdce885cba55b44e85c521385f9aa1445fcd8
guarheos@protonmail.com <guarheos@protonmail.com>
2025-05-07 20:14:31 -0400

    cambio de lenguaje según atributo

--- js/index.js
+++ js/index.js
@@ -1,33 +1,52 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('../reto3/conf/configES.json')
+    //Lenguaje dinamico
+    const parametrosurl = new URLSearchParams(window.location.search);
+    const lenguaje = parametrosurl.get("config");
+
+    if (lenguaje) {
+        const jsonurl = `../reto3/conf/config${lenguaje}.json`;
+
+        fetch(jsonurl)
+        .then(response => response.json())
+        .then(datos => {
+            //Poner datos del JSON al title y footer
+            document.title = datos.sitio.join(" ");
+            const footer = document.querySelector("footer");
+            footer.textContent= datos.copyRight;
+
+            //Poner datos a la barra superior, el saludo y la busqueda
+            document.getElementById("saludo").textContent=`${datos.saludo}, Guillermo Hernandez`;
+            document.getElementById("search").placeholder = `${datos.nombre}...`;
+            document.querySelector("button[type='submit']").textContent = datos.buscar;
+
+
+        })
+            .catch(error => console.error("Error al cargar la configuracion:", error));
+    } else {
+        console.error("No se proporcionó un ID de perfil.");
+    }
+
+
+    //Dummies dinamicos
+    fetch("../reto3/datos/index.json")
     .then(response => response.json())
-    .then(datos => {
-        //Poner datos del JSON al title y footer
-        document.title = datos.sitio.join(" ");
-        const footer = document.querySelector("footer");
-        footer.textContent= datos.copyRight;
+    .then(datosdummies => {
+        const dummies = document.getElementById("dummies");
+        //dummies.innerHTML = "";
+        datosdummies.forEach(dummy => {
+            const li = document.createElement("li");
+            li.classList.add("dummy");
 
-        //Dummies dinamicos
-        fetch("../reto3/datos/index.json")
-            .then(response => response.json())
-            .then(datosdummies => {
-                const dummies = document.getElementById("dummies");
-                //dummies.innerHTML = "";
-                datosdummies.forEach(dummy => {
-                    const li = document.createElement("li");
-                    li.classList.add("dummy");
+            li.innerHTML = `
+                <img src="${dummy.imagen}" alt="Foto de perfil">
+                <p class="nombreI">
+                    <a href="perfil.html?ci=${dummy.ci}">${dummy.nombre}</a>
+                </p>
+            `;
 
-                    li.innerHTML = `
-                        <img src="${dummy.imagen}" alt="Foto de perfil">
-                        <p class="nombreI">
-                            <a href="${dummy.perfil}">${dummy.nombre}</a>
-                        </p>
-                    `;
-
-                    dummies.appendChild(li);
-                });
-            })
-            .catch(error => console.error("Error cargando el JSON:", error));
-    });
+            dummies.appendChild(li);
+        });
+    })
+    .catch(error => console.error("Error cargando el JSON:", error));
 });
--- js/perfil.js
+++ js/perfil.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
+    //Perfil Dinamico
     const parametrosurl = new URLSearchParams(window.location.search);
-    const cedula = parametrosurl.get("ci"); // Obtiene el ID del perfil
-
+    const cedula = parametrosurl.get("ci");
     if (cedula) {
         const jsonurl = `../reto3/${cedula}/perfil.json`;
 
@@ -23,5 +23,29 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         console.error("No se proporcionó un ID de perfil.");
     }
+    
+        //Lenguaje dinamico
+        const parametrosurllenguaje = new URLSearchParams(window.location.search);
+        const lenguaje = parametrosurllenguaje.get("config");
+    
+        if (lenguaje) {
+            const jsonurl = `../reto3/conf/config${lenguaje}.json`;
+    
+            fetch(jsonurl)
+            .then(response => response.json())
+            .then(datos => {
+                document.getElementById("DefColor").textContent = datos.color;
+                document.getElementById("DefLibro").textContent = datos.libro;
+                document.getElementById("DefMusica").textContent = datos.musica;
+                document.getElementById("DefVideojuegos").textContent = datos.video_juego;
+                document.getElementById("DefLenguajes").innerHTML = `<b>${datos.lenguajes}</b>`;
+
+                document.getElementById("contacto").childNodes[0].textContent = datos.email.split("[email]")[0]; 
+                document.getElementById("contacto").childNodes[2].textContent = datos.email.split("[email]")[1]; 
+            })
+                .catch(error => console.error("Error al cargar la configuracion:", error));
+        } else {
+            console.error("No se proporcionó un ID de perfil.");
+        }
 });
 
